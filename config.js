84b4c6c506464af52bce4cc6fa6dd5270171088f
Murilo Polese <murilopolese@gmail.com>
2014-11-15 13:33:47 +0100

    Fix folder

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	repos: [
 		{
 			url: 'https://github.com/murilopolese/scriptcraftmods.git',
-			dest: '~/minecraft/plugins/scriptcraft/players/burrilove'
+			dest: '~/minecraft/plugins/scriptcraft/plugins/burrilove'
 		}
 	]
 };
