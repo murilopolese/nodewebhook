86a5c1bdfa59b3c9d99337ecd7b10f4ee73255c8
Murilo Polese <murilopolese@gmail.com>
2014-11-16 14:56:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,18 @@ var config = {
 		{
 			url: 'https://github.com/murilopolese/scriptcraftmods.git',
 			dest: '~/minecraft/plugins/scriptcraft/plugins/burrilove'
+		},
+		{
+			url: 'https://github.com/TheHackerOf2014/Mods.git',
+			dest: '~/minecraft/plugins/scriptcraft/plugins/ETG2'
+		},
+		{
+			url: 'https://github.com/isakmonka/My-Mod.git',
+			dest: '~/minecraft/plugins/scriptcraft/plugins/soderkisenpennan'
+		},
+		{
+			url: 'https://github.com/demo137/my-mods.git',
+			dest: '~/minecraft/plugins/scriptcraft/plugins/Aldin03'
 		}
 	]
 };
